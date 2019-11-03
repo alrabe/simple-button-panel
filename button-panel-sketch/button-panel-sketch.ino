9da78cd4011adbc8439c74e3607b5849f51ee03e
Alex Ben <alexander.bendel@gmail.com>
2019-11-03 15:26:30 +0100

    changed keypad button order; dim led by slider 1

--- button-panel-sketch/button-panel-sketch.ino
+++ button-panel-sketch/button-panel-sketch.ino
@@ -1,14 +1,14 @@
 #define DELAY_TIME 50
-#define LED_PIN 13
+#define LED 13
 #define SLIDER1 A0
 #define SLIDER2 A2
 #define KEYPAD A1
 
 const int keyPadResistorValues[] = {400, 500, 530, 560, 590, 630, 670, 720, 770, 830, 910, 1000};
-const int keyPadButtonIds[] = {12, 0, 11, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+const int keyPadButtonIds[] = {9, 5, 1, 10, 6, 2, 11, 7, 3, 12, 8, 4};
 
 void setup() {
-  pinMode(LED_PIN, OUTPUT);
+  pinMode(LED, OUTPUT);
   Serial.begin(19200);
 }
 
@@ -16,16 +16,22 @@ void loop() {
   unsigned long startTime = micros();
   ReportKeyPad();
   ReportSlider1();
-  digitalWrite(LED_PIN, HIGH);
-  delay(DELAY_TIME);
-  digitalWrite(LED_PIN, LOW);
-  unsigned long stopTime = micros();
+  DimLed();
+  delay(DELAY_TIME);  
+  //ReportTime(startTime, micros());
+}
 
+void ReportTime(long startTime, long stopTime) {
   // skip time overflow  
   if(stopTime > startTime) {
     Serial.print(stopTime - startTime);
     Serial.println("us");
-  }
+  }  
+}
+
+void DimLed() {
+  int brightness = map(analogRead(SLIDER1), 0, 1023, 0, 255);
+  analogWrite(LED, brightness);
 }
 
 void ReportKeyPad() {
